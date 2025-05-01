7bf44c3c2ee7869504231dfc823c0db4122a048b
Skill Issue <119864155+Rishab830@users.noreply.github.com>
2025-05-01 08:04:22 +0000

    filtered_java_earthquake

--- data_filter.py
+++ data_filter.py
@@ -1,4 +1,10 @@
 import os
 import obspy
-for file in os.listdir("Final_java_earthquake_data"):
+
+for folder in os.listdir("Final_java_earthquake_data"):
+    st = obspy.Stream()
+    for file in os.listdir(f"Final_java_earthquake_data/{folder}/waveforms"):
+        st += obspy.read(f"Final_java_earthquake_data/{folder}/waveforms/{file}")
+    st.plot()
     print(file)
+    
